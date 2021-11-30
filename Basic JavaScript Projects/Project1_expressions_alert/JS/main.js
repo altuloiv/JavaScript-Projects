529b915cb2ec28b5ac776555f8a88019f0a84b08
Albert Tulo <atuloiv@gmail.com>
2021-11-30 10:46:49 -0600

    initial commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+
+
+var A = "When the teacher saw me, she yelled, \"Stop Running in the Halls Al!\"";
+
+alert(A);
+
+var Dog = "Shadow", Mom = "Vicky", Dad = "Big Al", Son = "Lil Al", Daughter = "Paige";
+
+var Dog = Dog.fontcolor("blue");
+var Mom = Mom.fontcolor("purple");
+var Dad = Dad.fontcolor("green");
+var Son = Son.fontcolor("Red");
+var Daughter = Daughter.fontcolor("pink");
+
+document.write(Dog + Mom + Dad + Son + Daughter);
+
+document.write(4+4);
