63bc3ef2477cfa66c7e6b131f7346b207f00dae1
Albert Tulo <atuloiv@gmail.com>
2021-12-01 00:22:50 -0600

    added comments to code in main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -6,12 +6,12 @@ alert(A);
 
 var Dog = "Shadow", Mom = "Vicky", Dad = "Big Al", Son = "Lil Al", Daughter = "Paige";
 
-var Dog = Dog.fontcolor("blue");
-var Mom = Mom.fontcolor("purple");
-var Dad = Dad.fontcolor("green");
-var Son = Son.fontcolor("Red");
-var Daughter = Daughter.fontcolor("pink");
+var Dog = Dog.fontcolor("blue"); //making var Dog blue
+var Mom = Mom.fontcolor("purple"); //making var mom purple
+var Dad = Dad.fontcolor("green"); //making var dad green
+var Son = Son.fontcolor("Red"); //making var son red
+var Daughter = Daughter.fontcolor("pink"); //making var daughter pink
 
-document.write(Dog + Mom + Dad + Son + Daughter);
+document.write(Dog + Mom + Dad + Son + Daughter); //string adding all variables together
 
 document.write(4+4);
