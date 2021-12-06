94db49c7d0d2e2bb5c6bdea8a1704ffdfdbd087b
Albert Tulo <atuloiv@gmail.com>
2021-12-06 08:12:13 -0600

    finished project 10 for javascript

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,4 @@
-function count(){
+function count(){ //function that utilizes a while loop while X < 11, count up 1
     var Digit = "";
     var X = 1;
     while (X < 11) {
@@ -8,7 +8,7 @@ function count(){
     document.getElementById("Counting").innerHTML = Digit;
 }
 
-function string(){
+function string(){ //learning the let variable
     let text = "Cheese Pizza!";
     let length = text.length;
     document.getElementById("Length").innerHTML = length;
@@ -17,14 +17,14 @@ function string(){
 var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content = "";
 var Y;
-function for_Loop(){
+function for_Loop(){ //for loop
     for (Y = 0; Y < Instruments.length; Y++){
         Content += Instruments[Y] + "<br>";
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
 
-function cat_pics(){
+function cat_pics(){ //array
     var Cat_Picture = [];
     Cat_Picture[0] = "sleeping";
     Cat_Picture[1] = "playing";
@@ -33,14 +33,14 @@ function cat_pics(){
     document.getElementById("Cat").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
 }
 
-function constant_function(){
+function constant_function(){ //utilizing constant to make an object
     const Vehicle = {make: "Lamborhini", model:"Huracan",color: "verde-mantis"};
     Vehicle.color = "blue";
     Vehicle.price = "$300,000";
     document.getElementById("Constant").innerHTML = "The cost of the " + Vehicle.make + " was " + Vehicle.price;
 }
 
-function let_Keyword(){
+function let_Keyword(){ //let keywaord
     let car = "Lamborghini";
     document.getElementById("let").innerHTML = car;
 }
@@ -48,3 +48,38 @@ function let_Keyword(){
 function return_Function(){
     return document.getElementById("return").innerHTML = "wow it worked!";
 }
+
+let car = { //making an object with the let keyword
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "Red " , 
+    description : function(){
+        window.alert("This Car is a " + this.year + this.color +this.make + this.model);
+    }
+}; 
+car.description() ;
+
+
+function testBreak(x){ //learning how to use a break in the function to interupt a loop and go to the next step
+    var i = 0;
+    var x = 2;
+    while (i < 6){
+        if (i == 3){
+            break;
+        }
+        i += 1;
+    }
+    document.getElementById("break").innerHTML = i * x;
+}
+
+let text = '';
+
+for (let i = 0; i < 10; i++){ //learning how to use a continue keyword to continue throught a loop when something is met. 
+    if (i === 3) {
+        continue;
+    }
+    text = text + i;
+}
+
+console.log(text);
